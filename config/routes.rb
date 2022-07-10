b7dad6ec36bb1dcf39c73572f5a586eb3082a1fc
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-07-10 11:16:50 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :index, :show]
+
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
