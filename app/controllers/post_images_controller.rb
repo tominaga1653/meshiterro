bffd445c4746a66e8134420885e213b4a7b8c588
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-07-12 07:37:26 +0000

    [Finish] 4-19

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -30,7 +30,7 @@ class PostImagesController < ApplicationController
   private
 
   def post_image_params
-    params.require(:post_image).permit(:shop_name,:caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 
 end
